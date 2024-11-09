09b4e7f72433704cf72a185834fd40d75da4ad3c
Fabis-li <fabiano_slima@hotmail.com>
2024-11-08 21:16:51 -0300

    Atualizado endereço da API em Produção

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://note-keeper-api-u4ci.onrender.com/api',
+  API_URL: 'https://note-keeper-apii-fua6e2hce2ecd6ae.canadacentral-01.azurewebsites.net/api',
 };

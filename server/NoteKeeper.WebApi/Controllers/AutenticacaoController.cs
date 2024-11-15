691f35eadc47a078a7dc4835bf175e0203b14cfa
Fabis-li <fabiano_slima@hotmail.com>
2024-11-14 22:33:55 -0300

    criado servico de autenticacao

---
+++ server/NoteKeeper.WebApi/Controllers/AutenticacaoController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NoteKeeper.WebApi.ModuloAutenticacao;
+
+namespace NoteKeeper.WebApi.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AutenticacaoController : ControllerBase
+{
+    private readonly ServicoAutenticacao servicoAutenticacao;
+    private readonly IMapper mapeador;
+
+    public AutenticacaoController(ServicoAutenticacao servicoAutenticacao, IMapper mapeador)
+    {
+        this.servicoAutenticacao = servicoAutenticacao;
+        this.mapeador = mapeador;
+    }
+
+    [HttpPost("sair")]
+    public async Task<IActionResult> Sair()
+    {
+        await servicoAutenticacao.SairAsync();
+
+        return Ok();
+    }
+}
--- server/NoteKeeper.WebApi/Identity/IdentityDependecyInjection.cs
+++ server/NoteKeeper.WebApi/Identity/IdentityDependecyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using NoteKeeper.Dominio.ModuloAutenticacao;
 using NoteKeeper.Infra.Orm.Compartilhado;
+using NoteKeeper.WebApi.ModuloAutenticacao;
 
 namespace NoteKeeper.WebApi.Identity;
 
@@ -8,6 +9,7 @@ public static class IdentityDependecyInjection
 {
     public static void ConfigureIdentity(this IServiceCollection services)
     {
+        services.AddScoped<ServicoAutenticacao>();
         services.AddScoped<ITenantProvider, ApiTenantProvider>();
 
         services.AddIdentity<Usuario, Cargo>(options =>
@@ -16,7 +18,5 @@ public static class IdentityDependecyInjection
         })
         .AddEntityFrameworkStores<NoteKeeperDbContext>()
         .AddDefaultTokenProviders();
-
-
     }
 }
